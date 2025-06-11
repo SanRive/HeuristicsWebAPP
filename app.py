b14f380cb8f29ee729d6716ed3e05925ab47b907
Santiago Rivera Mejia <sriverame@unal.edu.co>
2025-06-11 00:53:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def leaderboard():
     return render_template("leaderboard.html", submissions=submissions)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', debug=False)
 
